b3c46f33b8a7b6eac7b13ec7295315c02a1a644f
Prem Acharya <prem.acharya@hotmail.com>
2021-03-19 13:11:25 +1100

    Password rest feature adeed

--- src/middlewares/formValidation.middleware.js
+++ src/middlewares/formValidation.middleware.js
@@ -1,94 +1,94 @@
 const Joi = require("joi");
 
 const email = Joi.string().email({
-  minDomainSegments: 2,
-  tlds: { allow: ["com", "net"] },
+	minDomainSegments: 2,
+	tlds: { allow: ["com", "net"] },
 });
 
 const pin = Joi.number().min(10000).max(999999).required();
 const phone = Joi.number().min(400000001).max(500000001).required();
 
-const newPassword = Joi.string().alphanum().min(3).max(30).required();
+const newPassword = Joi.string().min(3).max(30).required();
 
 const shortStr = Joi.string().min(2).max(50);
 const longStr = Joi.string().min(2).max(1000);
 const dt = Joi.date();
 
 const resetPassReqValidation = (req, res, next) => {
-  const schema = Joi.object({ email });
+	const schema = Joi.object({ email });
 
-  const value = schema.validate(req.body);
-  if (value.error) {
-    return res.json({ status: "error", message: value.error.message });
-  }
-  next();
+	const value = schema.validate(req.body);
+	if (value.error) {
+		return res.json({ status: "error", message: value.error.message });
+	}
+	next();
 };
 const updatePassValidation = (req, res, next) => {
-  const schema = Joi.object({ email, pin, newPassword });
+	const schema = Joi.object({ email, pin, newPassword });
 
-  const value = schema.validate(req.body);
-  if (value.error) {
-    return res.json({ status: "error", message: value.error.message });
-  }
-  next();
+	const value = schema.validate(req.body);
+	if (value.error) {
+		return res.json({ status: "error", message: value.error.message });
+	}
+	next();
 };
 
 const createNewTicketValidation = (req, res, next) => {
-  const schema = Joi.object({
-    subject: shortStr.required(),
-    sender: shortStr.required(),
-    message: longStr.required(),
-    issueDate: dt.required(),
-  });
+	const schema = Joi.object({
+		subject: shortStr.required(),
+		sender: shortStr.required(),
+		message: longStr.required(),
+		issueDate: dt.required(),
+	});
 
-  console.log(req.body);
-  const value = schema.validate(req.body);
+	console.log(req.body);
+	const value = schema.validate(req.body);
 
-  if (value.error) {
-    return res.json({ status: "error", message: value.error.message });
-  }
+	if (value.error) {
+		return res.json({ status: "error", message: value.error.message });
+	}
 
-  next();
+	next();
 };
 const replyTicketMessageValidation = (req, res, next) => {
-  const schema = Joi.object({
-    sender: shortStr.required(),
-    message: longStr.required(),
-  });
+	const schema = Joi.object({
+		sender: shortStr.required(),
+		message: longStr.required(),
+	});
 
-  console.log(req.body);
-  const value = schema.validate(req.body);
+	console.log(req.body);
+	const value = schema.validate(req.body);
 
-  if (value.error) {
-    return res.json({ status: "error", message: value.error.message });
-  }
+	if (value.error) {
+		return res.json({ status: "error", message: value.error.message });
+	}
 
-  next();
+	next();
 };
 
 const newUserValidation = (req, res, next) => {
-  const schema = Joi.object({
-    name: shortStr.required(),
-    company: shortStr.required(),
-    address: shortStr.required(),
-    phone: phone,
-    email: shortStr.required(),
-    password: shortStr.required(),
-  });
+	const schema = Joi.object({
+		name: shortStr.required(),
+		company: shortStr.required(),
+		address: shortStr.required(),
+		phone: phone,
+		email: shortStr.required(),
+		password: shortStr.required(),
+	});
 
-  const value = schema.validate(req.body);
+	const value = schema.validate(req.body);
 
-  if (value.error) {
-    return res.json({ status: "error", message: value.error.message });
-  }
+	if (value.error) {
+		return res.json({ status: "error", message: value.error.message });
+	}
 
-  next();
+	next();
 };
 
 module.exports = {
-  resetPassReqValidation,
-  updatePassValidation,
-  createNewTicketValidation,
-  replyTicketMessageValidation,
-  newUserValidation,
+	resetPassReqValidation,
+	updatePassValidation,
+	createNewTicketValidation,
+	replyTicketMessageValidation,
+	newUserValidation,
 };
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -3,28 +3,28 @@ const { route, post } = require("./ticket.router");
 const router = express.Router();
 
 const {
-  insertUser,
-  getUserByEmail,
-  getUserById,
-  updatePassword,
-  storeUserRefreshJWT,
-  verifyUser,
+	insertUser,
+	getUserByEmail,
+	getUserById,
+	updatePassword,
+	storeUserRefreshJWT,
+	verifyUser,
 } = require("../model/user/User.model");
 const { hashPassword, comparePassword } = require("../helpers/bcrypt.helper");
 const { crateAccessJWT, crateRefreshJWT } = require("../helpers/jwt.helper");
 const {
-  userAuthorization,
+	userAuthorization,
 } = require("../middlewares/authorization.middleware");
 const {
-  setPasswordRestPin,
-  getPinByEmailPin,
-  deletePin,
+	setPasswordRestPin,
+	getPinByEmailPin,
+	deletePin,
 } = require("../model/restPin/RestPin.model");
 const { emailProcessor } = require("../helpers/email.helper");
 const {
-  resetPassReqValidation,
-  updatePassValidation,
-  newUserValidation,
+	resetPassReqValidation,
+	updatePassValidation,
+	newUserValidation,
 } = require("../middlewares/formValidation.middleware");
 const { verify } = require("jsonwebtoken");
 
@@ -33,225 +33,219 @@ const { deleteJWT } = require("../helpers/redis.helper");
 const verificationURL = "http://localhost:3000/verification/";
 
 router.all("/", (req, res, next) => {
-  // res.json({ message: "return form user router" });
+	// res.json({ message: "return form user router" });
 
-  next();
+	next();
 });
 
 // Get user profile router
 router.get("/", userAuthorization, async (req, res) => {
-  //this data coming form database
-  const _id = req.userId;
+	//this data coming form database
+	const _id = req.userId;
 
-  const userProf = await getUserById(_id);
-  const { name, email } = userProf;
-  res.json({
-    user: {
-      _id,
-      name,
-      email,
-    },
-  });
+	const userProf = await getUserById(_id);
+	const { name, email } = userProf;
+	res.json({
+		user: {
+			_id,
+			name,
+			email,
+		},
+	});
 });
 
 ///very user after user is sign up
 router.patch("/verify", async (req, res) => {
-  try {
-    const { _id, email } = req.body;
-    console.log(_id, email);
+	try {
+		const { _id, email } = req.body;
+		console.log(_id, email);
 
-    const result = await verifyUser(_id, email);
+		const result = await verifyUser(_id, email);
 
-    if (result && result.id) {
-      return res.json({
-        status: "success",
-        message: "You account has been activated, you may sign in now.",
-      });
-    }
+		if (result && result.id) {
+			return res.json({
+				status: "success",
+				message: "You account has been activated, you may sign in now.",
+			});
+		}
 
-    return res.json({
-      status: "error",
-      message: "Invalid request!",
-    });
-  } catch (error) {
-    console.log(error);
-    return res.json({
-      status: "error",
-      message: "Invalid request!",
-    });
-  }
+		return res.json({
+			status: "error",
+			message: "Invalid request!",
+		});
+	} catch (error) {
+		console.log(error);
+		return res.json({
+			status: "error",
+			message: "Invalid request!",
+		});
+	}
 });
 
 // Create new user router
 router.post("/", newUserValidation, async (req, res) => {
-  const { name, company, address, phone, email, password } = req.body;
+	const { name, company, address, phone, email, password } = req.body;
 
-  try {
-    //hash password
-    const hashedPass = await hashPassword(password);
+	try {
+		//hash password
+		const hashedPass = await hashPassword(password);
 
-    const newUserObj = {
-      name,
-      company,
-      address,
-      phone,
-      email,
-      password: hashedPass,
-    };
-    const result = await insertUser(newUserObj);
-    console.log(result);
+		const newUserObj = {
+			name,
+			company,
+			address,
+			phone,
+			email,
+			password: hashedPass,
+		};
+		const result = await insertUser(newUserObj);
+		console.log(result);
 
-    await emailProcessor({
-      email,
-      type: "new-user-confirmation-required",
-      verificationLink: verificationURL + result._id + "/" + email,
-    });
+		await emailProcessor({
+			email,
+			type: "new-user-confirmation-required",
+			verificationLink: verificationURL + result._id + "/" + email,
+		});
 
-    res.json({ status: "success", message: "New user created", result });
-  } catch (error) {
-    console.log(error);
+		res.json({ status: "success", message: "New user created", result });
+	} catch (error) {
+		console.log(error);
 
-    let message =
-      "Unable to create new user at the moment, Please try agin or contact administration!";
-    if (error.message.includes("duplicate key error collection")) {
-      message = "this email already has an account";
-    }
-    res.json({ status: "error", message });
-  }
+		let message =
+			"Unable to create new user at the moment, Please try agin or contact administration!";
+		if (error.message.includes("duplicate key error collection")) {
+			message = "this email already has an account";
+		}
+		res.json({ status: "error", message });
+	}
 });
 
 //User sign in Router
 router.post("/login", async (req, res) => {
-  console.log(req.body);
+	console.log(req.body);
 
-  const { email, password } = req.body;
+	const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res.json({ status: "error", message: "Invalid form submition!" });
-  }
+	if (!email || !password) {
+		return res.json({ status: "error", message: "Invalid form submition!" });
+	}
 
-  const user = await getUserByEmail(email);
+	const user = await getUserByEmail(email);
 
-  if (!user.isVerified) {
-    return res.json({
-      status: "error",
-      message:
-        "You account has not been verified. Please check your email and verify you account before able to login!",
-    });
-  }
+	if (!user.isVerified) {
+		return res.json({
+			status: "error",
+			message:
+				"You account has not been verified. Please check your email and verify you account before able to login!",
+		});
+	}
 
-  const passFromDb = user && user._id ? user.password : null;
+	const passFromDb = user && user._id ? user.password : null;
 
-  if (!passFromDb)
-    return res.json({ status: "error", message: "Invalid email or password!" });
+	if (!passFromDb)
+		return res.json({ status: "error", message: "Invalid email or password!" });
 
-  const result = await comparePassword(password, passFromDb);
+	const result = await comparePassword(password, passFromDb);
 
-  if (!result) {
-    return res.json({ status: "error", message: "Invalid email or password!" });
-  }
+	if (!result) {
+		return res.json({ status: "error", message: "Invalid email or password!" });
+	}
 
-  const accessJWT = await crateAccessJWT(user.email, `${user._id}`);
-  const refreshJWT = await crateRefreshJWT(user.email, `${user._id}`);
+	const accessJWT = await crateAccessJWT(user.email, `${user._id}`);
+	const refreshJWT = await crateRefreshJWT(user.email, `${user._id}`);
 
-  res.json({
-    status: "success",
-    message: "Login Successfully!",
-    accessJWT,
-    refreshJWT,
-  });
+	res.json({
+		status: "success",
+		message: "Login Successfully!",
+		accessJWT,
+		refreshJWT,
+	});
 });
 
 router.post("/reset-password", resetPassReqValidation, async (req, res) => {
-  const { email } = req.body;
+	const { email } = req.body;
 
-  const user = await getUserByEmail(email);
+	const user = await getUserByEmail(email);
 
-  if (user && user._id) {
-    /// crate// 2. create unique 6 digit pin
-    const setPin = await setPasswordRestPin(email);
-    await emailProcessor({
-      email,
-      pin: setPin.pin,
-      type: "request-new-password",
-    });
+	if (user && user._id) {
+		/// crate// 2. create unique 6 digit pin
+		const setPin = await setPasswordRestPin(email);
+		await emailProcessor({
+			email,
+			pin: setPin.pin,
+			type: "request-new-password",
+		});
+	}
 
-    return res.json({
-      status: "success",
-      message:
-        "If the email is exist in our database, the password reset pin will be sent shortly.",
-    });
-  }
-
-  res.json({
-    status: "error",
-    message:
-      "If the email is exist in our database, the password reset pin will be sent shortly.",
-  });
+	res.json({
+		status: "success",
+		message:
+			"If the email is exist in our database, the password reset pin will be sent shortly.",
+	});
 });
 
 router.patch("/reset-password", updatePassValidation, async (req, res) => {
-  const { email, pin, newPassword } = req.body;
+	const { email, pin, newPassword } = req.body;
 
-  const getPin = await getPinByEmailPin(email, pin);
-  // 2. validate pin
-  if (getPin._id) {
-    const dbDate = getPin.addedAt;
-    const expiresIn = 1;
+	const getPin = await getPinByEmailPin(email, pin);
+	// 2. validate pin
+	if (getPin?._id) {
+		const dbDate = getPin.addedAt;
+		const expiresIn = 1;
 
-    let expDate = dbDate.setDate(dbDate.getDate() + expiresIn);
+		let expDate = dbDate.setDate(dbDate.getDate() + expiresIn);
 
-    const today = new Date();
+		const today = new Date();
 
-    if (today > expDate) {
-      return res.json({ status: "error", message: "Invalid or expired pin." });
-    }
+		if (today > expDate) {
+			return res.json({ status: "error", message: "Invalid or expired pin." });
+		}
 
-    // encrypt new password
-    const hashedPass = await hashPassword(newPassword);
+		// encrypt new password
+		const hashedPass = await hashPassword(newPassword);
 
-    const user = await updatePassword(email, hashedPass);
+		const user = await updatePassword(email, hashedPass);
 
-    if (user._id) {
-      // send email notification
-      await emailProcessor({ email, type: "update-password-success" });
+		if (user._id) {
+			// send email notification
+			await emailProcessor({ email, type: "update-password-success" });
 
-      ////delete pin from db
-      deletePin(email, pin);
+			////delete pin from db
+			deletePin(email, pin);
 
-      return res.json({
-        status: "success",
-        message: "Your password has been updated",
-      });
-    }
-  }
-  res.json({
-    status: "error",
-    message: "Unable to update your password. plz try again later",
-  });
+			return res.json({
+				status: "success",
+				message: "Your password has been updated",
+			});
+		}
+	}
+	res.json({
+		status: "error",
+		message: "Unable to update your password. plz try again later",
+	});
 });
 
 // User logout and invalidate jwts
 
 router.delete("/logout", userAuthorization, async (req, res) => {
-  const { authorization } = req.headers;
-  //this data coming form database
-  const _id = req.userId;
+	const { authorization } = req.headers;
+	//this data coming form database
+	const _id = req.userId;
 
-  // 2. delete accessJWT from redis database
-  deleteJWT(authorization);
+	// 2. delete accessJWT from redis database
+	deleteJWT(authorization);
 
-  // 3. delete refreshJWT from mongodb
-  const result = await storeUserRefreshJWT(_id, "");
+	// 3. delete refreshJWT from mongodb
+	const result = await storeUserRefreshJWT(_id, "");
 
-  if (result._id) {
-    return res.json({ status: "success", message: "Loged out successfully" });
-  }
+	if (result._id) {
+		return res.json({ status: "success", message: "Loged out successfully" });
+	}
 
-  res.json({
-    status: "error",
-    message: "Unable to logg you out, plz try again later",
-  });
+	res.json({
+		status: "error",
+		message: "Unable to logg you out, plz try again later",
+	});
 });
 
 module.exports = router;
